c725389a01b4b18f5962e6340ca6ab6e4cb4a3eb
salgue441 <carlossalguero441@gmail.com>
2023-01-11 23:35:32 -0600

    Implemented new features

--- main.cpp
+++ main.cpp
@@ -2,46 +2,174 @@
 #include <random>
 #include <ranges>
 #include <algorithm>
+#include <limits>
 
-// SDL libraries
+// SDL library
 #include <SDL2/SDL.h>
+
+// Function prototypes
+void menu();
+void draw_states(std::vector<int> &, SDL_Renderer *,
+                 unsigned int, unsigned int);
+
+int validate_input(int);
+void end_program();
 
 // Sorting Algorithms
 
 int main(int argc, char **argv)
 {
-    std::random_device rng;
-    std::uniform_int_distribution d(1, 99);
+    // Unused argc, argv
+    (void)argc;
+    (void)argv;
 
-    std::vector<int> data_vector;
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        std::cout << "Error: SDL2 could not be initialized" << std::endl;
+        printf("SDL ERROR: %s", SDL_GetError());
 
-    // Fill the vector with random numbers
-    for (int i{}; i < 100; ++i)
-        data_vector.push_back(d(rng));
+        return EXIT_FAILURE;
+    }
 
-    // SDL window
-    SDL_Window *window = nullptr;
-    SDL_Renderer *renderer = nullptr;
+    // Window
+    const int window_width{500};
+    const int window_height{500};
 
-    SDL_CreateWindowAndRenderer(
-        100 * 10, 100 * 10, 0,
-        &window, &renderer);
+    SDL_Window *window{SDL_CreateWindow("Sorting Algorithm Visualizer",
+                                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                        window_width, window_height,
+                                        0)};
+
+    if (!window)
+    {
+        std::cout << "Error: window could not be initialized\n";
+        printf("SDL ERROR: %s", SDL_GetError());
+
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer *renderer{SDL_CreateRenderer(window, -1,
+                                              SDL_RENDERER_SOFTWARE |
+                                                  SDL_RENDERER_PRESENTVSYNC)};
+
+    if (renderer == nullptr || !renderer)
+    {
+        std::cout << "Error: could not create renderer for main window."
+                  << std::endl;
+        printf("SDL ERROR: %s", SDL_GetError());
+
+        return EXIT_FAILURE;
+    }
 
     SDL_RenderSetScale(renderer, 10, 10);
 
+    std::random_device rng;
+    std::uniform_int_distribution d(1, 99);
+
+    std::vector<int> data_vec;
+
+    // Filling the data
+    for (int i{}; i < 100; ++i)
+        data_vec.push_back(d(rng));
+
     // Sorting
-    for (unsigned int i{}; i < data_vector.size(); ++i)
-        for (unsigned j{i}; j < data_vector.size(); ++j)
-            if (data_vector[j] < data_vector[i])
-                std::swap(data_vector[j], data_vector[i]);
+    bool running{true};
 
-    // Print the vector
-    for (auto i : data_vector)
-        std::cout << i << " ";
+    while (running)
+    {
+        SDL_Event event;
 
-    std::cout << std::endl;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
 
-    (std::ranges::is_sorted(data_vector))
-        ? std::cout << "Sorted \n"
-        : std::cout << "Not sorted \n";
+                break;
+            }
+        }
+
+        for (unsigned int i{}; i < data_vec.size(); ++i)
+            for (unsigned j{i}; j < data_vec.size(); ++j)
+                if (data_vec[j] < data_vec[i])
+                    std::swap(data_vec[j], data_vec[i]);
+    }
+}
+
+// Function Declaration
+/**
+ * @brief
+ * Shows a simple menu to the user
+ */
+void menu()
+{
+    std::cout << "---- Sorting Algorithms Visualizer ----" << std::endl;
+
+    std::cout << "1. Bubble Sort Algorithm \n2. Selection Sort Algorithm"
+              << "\n3. Insertion Sort Algorithm \n4. Quick Sort Algorithm"
+              << "\n5. Merge Sort Algorithm \n6. Quit Application" << std::endl;
+}
+
+/**
+ * @brief
+ * Draw the states of the sorting algorithm in the screen.
+ * @param data_vec Vector with the data
+ * @param renderer Renderer where the render happens
+ * @param red Red color
+ * @param blue Blue color
+ */
+void draw_states(std::vector<int> &data_vec, SDL_Renderer *renderer,
+                 unsigned int red, unsigned int blue)
+{
+    int index{};
+
+    for (auto i : data_vec)
+    {
+        if (index == red)
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+        else if (index == blue)
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+        else
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        SDL_RenderDrawLine(renderer, index, 99, index, i);
+    }
+}
+
+/**
+ * @brief
+ * Validates the user input. If the input is correct, returns the value.
+ * Otherwise, asks the user for another input.
+ * @param input User input to be validated
+ * @return int Input validated.
+ */
+int validate_input(int input)
+{
+    while (!(std::cin >> input))
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),
+                        '\n');
+
+        std::cout << "Invalid input. Try again: ";
+    }
+
+    return input;
+}
+
+/**
+ * @brief
+ * Ends the program.
+ * @param window Window to be destroyed
+ * @param renderer Renderer to be destroyed.
+ */
+void end_program()
+{
+    SDL_DestroyWindow(SDL_GL_GetCurrentWindow());
+    SDL_DestroyRenderer(
+        SDL_GetRenderer(SDL_GL_GetCurrentWindow()));
+
+    SDL_Quit();
 }
